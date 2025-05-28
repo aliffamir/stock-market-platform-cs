b40ae754631c07bccf97c770d54ae3e004c563f8
Aliff Amir <aliff6406@gmail.com>
2025-05-28 14:53:05 +0100

    feat: add Comment model properties

--- api/api/Models/Comment.cs
+++ api/api/Models/Comment.cs
@@ -2,6 +2,12 @@ namespace api.Models;
 
 public class Comment
 {
-    public string CommentContent { get; set; } = string.Empty;
-    public int Likes { get; set; }
+    public int Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Content { get; set; } = string.Empty;
+    public DateTime CreatedOn { get; set; } = DateTime.Now;
+    // Foreign Key
+    public int? StockId { get; set; }
+    // Navigation property
+    public Stock? Stock { get; set; }
 }
