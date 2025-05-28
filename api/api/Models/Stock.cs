faed7cba131f1e3196dcd66f5ba3f47ab773f5fd
Aliff Amir <aliff6406@gmail.com>
2025-05-28 14:36:08 +0100

    feat: add Stock model

--- api/api/Models/Stock.cs
+++ api/api/Models/Stock.cs
@@ -1,6 +1,18 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace api.Models;
 
 public class Stock
 {
+    public int Id { get; set; }
+    public string Symbol { get; set; } = string.Empty;
+    public string CompanyName { get; set; } = string.Empty;
     
+    // limit to 2 decimal places
+    [Column(TypeName = "decimal(18,2)")] public decimal MyProperty { get; set; }
+    public decimal Purchase { get; set; }
+    [Column(TypeName = "decimal(18,2)")] public decimal LastDividend { get; set; }
+    public string Industry { get; set; } = string.Empty;
+    public long MarketCap { get; set; }
+    public List<Comment> Comments { get; set; } = new List<Comment>();
 }
