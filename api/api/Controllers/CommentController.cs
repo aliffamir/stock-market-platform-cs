e7aad0a35d632800ec4ed2edc79464751d924bca
Aliff Amir <aliff6406@gmail.com>
2025-06-02 14:14:16 +0100

    add get all comments endpoint

---
+++ api/api/Controllers/CommentController.cs
@@ -0,0 +1,25 @@
+using api.Interfaces;
+using api.Mappers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controllers;
+
+[Route("api/comment")]
+[ApiController]
+public class CommentController : ControllerBase 
+{
+   private readonly ICommentRepository _commentRepo;
+
+   public CommentController(ICommentRepository commentRepo)
+   {
+      _commentRepo = commentRepo;
+   }
+
+   [HttpGet]
+   public async Task<IActionResult> GetAll()
+   {
+      var comments = await _commentRepo.GetAllAsync();
+      var commentDto = comments.Select(comment => comment.ToCommentDto());
+      return Ok(commentDto);
+   }
+}
--- api/api/Program.cs
+++ api/api/Program.cs
@@ -26,6 +26,7 @@ public class Program
         });
 
         builder.Services.AddScoped<IStockRepository, StockRepository>();
+        builder.Services.AddScoped<ICommentRepository, CommentRepository>();
 
         var app = builder.Build();
 
